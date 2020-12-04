a4c0c214ffe47790a6f31590d21871958c3fd172
PorriDG3 <gn02053231@gmail.com>
2020-12-04 09:28:47 +0800

    fix again

--- KnstAsyncApiUI/app.js
+++ KnstAsyncApiUI/app.js
@@ -3,7 +3,7 @@ const Generator = require('@asyncapi/generator');
 
 async function generateAsyncApi(asyncapiDocument, directory) {
     try {
-        let generator = new Generator('@asyncapi/html-template', path.resolve(__dirname, directory), { forceWrite=true });
+        let generator = new Generator('@asyncapi/html-template', path.resolve(__dirname, directory), { forceWrite: true });
         await generator.generateFromString(asyncapiDocument);
     } catch (e) {
         console.error(e);
